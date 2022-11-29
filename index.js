ed494fbc654b7a5710cb6f09f765c9efe4762498
Toru Ishiguro <t-ishiguro@b-tm.co.jp>
2022-11-29 20:41:07 +0900

    Add files

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+// カウンター機能用
+let counter = 1;
+
+app.get("/", (req, res, next) => {
+  res.send(`あなたは${counter}人目のお客様です。`);
+  counter++;
+})
+
+app.get("/reset", (req, res, next) => {
+  res.send("リセットしました。")
+  counter = 1;
+})
+
+app.listen(8000, () => console.log('Example app listening on port 8000!'))
