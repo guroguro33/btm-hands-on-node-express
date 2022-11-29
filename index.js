5d880be83fd0421d69faecfcd7529f45a866bb1c
Toru Ishiguro <t-ishiguro@b-tm.co.jp>
2022-11-29 21:01:08 +0900

    Add kiri number

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express();
 // カウンター機能用
 let counter = 1;
 
+// キリ番用数字
+const kiri_number = 10; 
+
 app.get("/", (req, res, next) => {
-  res.send(`あなたは${counter}人目のお客様です。`);
+  let over = counter % kiri_number;
+  if (over === 0) {
+    res.send(`おめでとうございます。あなたは${counter}人目のお客様です。`);
+  } else {
+    res.send(`あなたは${counter}人目のお客様です。キリ番まであと${kiri_number - over}人です。`);
+  }
   counter++;
 })
 
